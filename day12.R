66279c82cf6e99a4c912614fe03ddd1e1f056edb
Cetin_G <ceting@gmx.at>
2024-12-12 22:24:32 +0100

    Improved day 12

--- day12.R
+++ day12.R
@@ -24,22 +24,23 @@ find_region <- function(cur) {
 
 compute_price <- function(reg) {
  
-  rw <- (reg - 1L) %%  n + 1L
-  cl <- (reg - 1L) %/% n + 1L
+  rw <- (reg - 1L) %%  n + 1L # row
+  cl <- (reg - 1L) %/% n + 1L # column
   
   sides <- 0L
-  for (k in seq.int(min(rw) - 1L, max(rw) + 1L)) {
+  for (k in seq.int(min(rw) - 1L, max(rw))) {
     x1 <- cl[rw == k]
     x2 <- cl[rw == k + 1L]
     
-    x12 <- setdiff(x1, x2)
-    x21 <- setdiff(x2, x1)
+    x12 <- x1[match(x1, x2, 0L) == 0L]
+    x21 <- x2[match(x2, x1, 0L) == 0L]
     
     hside <- (length(x12) > 0) + (length(x21) > 0) + sum(diff(x12) > 1) + sum(diff(x21) > 1)
     
     sides <- sides + hside * 2L
   }
   
+  #area x (perimeter, number of sides)
   length(reg) * c(4L * length(reg) - length(unlist(lookup[reg])), sides)
   
 }
