76469295e110528cdabf552d6651d0d8607ca0aa
Cetin_G <ceting@gmx.at>
2024-12-14 16:02:34 +0100

    clean solution day 14

--- day14.R
+++ day14.R
@@ -15,22 +15,11 @@ prod(table(q[q!=0]))
 
 
 # part 2---------------
-y <- x
+sd_vec <- sapply(1:103, \(k) c(sd((x[1,] + x[3,] * k) %% 101L), sd(((x[2,] + x[4,] * k) %% 103L))))
+x0 <- apply(sd_vec, 1, which.min)
 
-# for (k in 1:10403) {
-# 
-#   y[1:2, ] <- rbind((y[1,] + y[3,] * 1L) %% 101L, (y[2,] + y[4,] * 1L) %% 103L)
-#   if (sum(sapply(split(y[1, ], y[2, ]), \(a) sum(diff(sort(a)) == 1)) > 20) > 2) print(k)
-#   
-#   if(sum(diff(lengths(split(y[1, ], y[2,]))) > 0) >= 40) print(k)
-# }
+k <- which(((x0[1] + 1:100 * 101L) %% 103L) == x0[2])*101L + x0[1]
 
-f <- function(k) {
-  y <- cbind((x[1,] + x[3,] * k) %% 101L, -((x[2,] + x[4,] * k) %% 103L))
-  plot(y, pch = 15, cex = 2, xlim = c(-3, 103), ylim = c(-103, 3),  main = k)
-  }
 
-#manual search :/
-pdf("day14.pdf")
-sapply(7000:8000, f)
-dev.off()
+plot(cbind((x[1,] + x[3,] * k) %% 101L, -(x[2,] + x[4,] * k) %% 103L), pch = 15)
+
