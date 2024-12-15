5e016b4defb74fa0b7f8fd782fb276550fd050a6
Cetin_G <ceting@gmx.at>
2024-12-15 12:45:23 +0100

    First solution day 15

---
+++ day15.R
@@ -0,0 +1,93 @@
+data15 <- readLines("Input/day15.txt")
+n <- 50L
+dir_vec <- unname(c("^" = -1i, "v" = 1i, ">" = 1, "<" = -1)[unlist(strsplit(data15[-(1:(n + 1))],""))])
+
+x <- as.character(do.call(rbind, strsplit(data15[1:n], "")))
+co <- rep(seq_len(n) - 1, each = n) + rep(seq_len(n) - 1, n) * 1i
+
+wall <- co[x == "#"]
+box <- co[x == "O"]
+pos <- co[x == "@"]
+
+
+for (dir in dir_vec) {
+   new_pos <- pos + dir
+     if (!new_pos %in% wall) {
+         box2 <- if (Im(dir) == 0) box[Im(box) == Im(pos)] else box[Re(box) == Re(pos)]
+         if (new_pos %in% box2) {
+          n_box <- Position(\(k) !(pos + k * dir) %in% box2, 1:n) - 1L
+          if (!(pos + (n_box + 1L) * dir) %in% wall) {
+              mbox <- pos + seq_len(n_box) * dir #boxes to be moved
+              box[box %in% mbox] <- mbox + dir 
+              pos <- new_pos
+          }
+         }
+         else pos <- new_pos
+     }
+}
+
+sum(100 * Im(box) + Re(box))
+
+
+#part 2---------
+update_tile <- function(x) {
+    gsub("#", "##", gsub("O", "[]", gsub("@", "@.", gsub("\\.", "..", x))))
+}
+
+x <- as.character(do.call(rbind, strsplit(update_tile(data15[1:n]), "")))
+co <- rep(seq_len(2 * n) - 1, each = n) + rep((seq_len(n) - 1), 2 * n) * 1i
+wall <- co[x == "#"]
+box_l <- co[x == "["]
+box_r <- box_l + 1
+box <- c(box_l, box_r)
+pos <- co[x == "@"]
+
+for (dir in dir_vec) {
+    new_pos <- pos + dir
+    if (!new_pos %in% wall) {
+        if (Im(dir) == 0) { #left right
+            box2 <- box[Im(box) == Im(pos)]
+            if (new_pos %in% box2) {
+                n_box <- Position(\(k) !(pos + k * dir) %in% box2, 1:n) - 1L
+                if (!(pos + (n_box + 1L) * dir) %in% wall) {
+                    mbox <- pos + seq_len(n_box) * dir #boxes to be moved
+                    box_l[box_l %in% mbox] <- box_l[box_l %in% mbox] + dir
+                    box_r[box_r %in% mbox] <- box_r[box_r %in% mbox] + dir
+                    box <- c(box_l, box_r)
+                    pos <- new_pos
+                }
+            }
+            else pos <- new_pos
+        } else { # up down 
+            box2 <- box[Re(box) == Re(pos)]
+            if (new_pos %in% box2) {
+                mbox <- new_pos + c(0, if (new_pos %in% box_l) 1 else -1)
+                move <- TRUE
+                n_box <- mbox
+                while(TRUE) {
+                    if (any((n_box + dir) %in% wall)) {
+                        move <- FALSE
+                        break
+                    }
+                    n_box <- box[box %in% (n_box + dir)]
+                    if (length(n_box) == 0) break
+                    n_box <- n_box[order(Re(n_box))]
+                    if (n_box[1] %in% box_r) {n_box <- c(n_box[1] - 1, n_box)}
+                    if (n_box[length(n_box)] %in% box_l) n_box <- c(n_box, n_box[length(n_box)] + 1)
+                    mbox <- c(mbox, n_box)
+                }
+                
+                if (move) {
+                    box_l[box_l %in% mbox] <- box_l[box_l %in% mbox] + dir
+                    box_r[box_r %in% mbox] <- box_r[box_r %in% mbox] + dir
+                    box <- c(box_l, box_r)
+                    pos <- new_pos
+                }
+                
+            }
+            else pos <- new_pos
+        }
+    }
+}
+
+sum(100 * Im(box_l) + Re(box_r) - 1)
