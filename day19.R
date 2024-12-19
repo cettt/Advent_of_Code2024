692923621b27822798dc721f3d41fd2fac7269b6
Cetin_G <ceting@gmx.at>
2024-12-19 22:15:27 +0100

    Efficient version of day 19

--- day19.R
+++ day19.R
@@ -2,57 +2,43 @@ data19 <- readLines("Input/day19.txt")
 
 twl <- strsplit(data19[1], ", ")[[1]]
 pat_vec <- data19[-(1:2)]
-sing <- c("w", "b", "u", "r", "g")
 
-# missing single stripe
-x1 <- setdiff(sing, twl) #missing single color: here w
-
-# missing combinations of length two with single stripe: here rw 
-x2 <- setdiff(c(paste0(sing, x1), paste0(x1, sing)), twl)
-
-# the only pattern of length two which is impossible is rw
-#  so whenever a pattern does not end in rw there is a possible combination
-
-
-if (grepl(paste0(x1, "$"), x2)) {
-  twl2 <- paste0(twl, "$")
-  x3 <- paste0(x2, "$")
-} else {
-  twl2 <- paste0("^", twl)
-  x3 <- paste0("^", x2)
-}
-
-check_pat <- function(pat) {
-  if (nchar(pat) == 0) return(TRUE)
-  if (!grepl(x3, pat)) return(TRUE)
-  
-  idx <- str_detect(pat, twl2)
-  if (!any(idx)) return(FALSE)
-  
-  any(sapply(str_replace(pat, twl2[idx], ""), check_pat))
-}
-
-
-pos_idx <- sapply(pat_vec, check_pat)
-sum(pos_idx)
-
-
-# part 2----------
-mem <- integer()
+twl_list <- split(twl, nchar(twl))
+nc <- sort(unique(nchar(twl)))
 
 count_pat <- function(pat) {
-  if (nchar(pat) == 0) return(1)
-  if (pat %in% names(mem)) return(mem[pat])
   
-  idx <- str_detect(pat, twl2)
-  if (!any(idx)) return(0)
+  res <- numeric(nchar(pat) + 1L)
+  res[1] <- 1
   
-  res <- sum(sapply(str_replace(pat, twl2[idx], ""), count_pat))
-  mem <<- c(mem, setNames(res, pat))
-  return(res)
+  for (j in seq_len(nchar(pat))) {
+    twl_list2 <- twl_list[nc <= j]
+    for (i in seq_along(twl_list2)) {
+      if (any(substr(pat, j + 1L - nc[i], j) == twl_list2[[i]])) {
+        res[j + 1L] <- res[j + 1L] + res[j + 1 - nc[i]]
+      }
+    }
+  }
+  res[length(res)]
+  
 }
 
-invisible(sapply(twl, count_pat)) # create simple combinations
+res <- sapply(pat_vec, count_pat)
 
-sprintf("%.f", sum(sapply(pat_vec[pos_idx], count_pat)))
+# part 1-----------------
+sum(res > 0)
 
+# part 2-----------------
+sprintf("%.f", sum(res))
+
+
+# explanation----------
+# in order to count the number of patterns we use recursion:
+#  res[j] contains the number of valid patterns for the substring containing the first j - 1 letters
+#    by default res[1] is the empty string and there is only one combination to obtain it
+# To illustrate the recursion consider the string ABCDE
+#  -  First we check if any pattern matches the first letter: if yes, we set res[2] <- res[2] + res[1] = 1
+#    Otherwise res[2] stays at zero.
+#  - Next we check the first two letters AB
+#     if B is matched then res[3] <- res[3] + res[2]
+#     if AB is matched as well, we set res[3] <- res[3] + res[1]
