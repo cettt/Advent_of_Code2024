aff9f706d79e78247ead8798c918611f7a8ef376
Cetin_G <ceting@gmx.at>
2024-12-25 21:22:55 +0100

    Improved day06

--- day06.R
+++ day06.R
@@ -1,108 +1,80 @@
-n <- 130
-x <- read.fwf("Input/day06.txt", widths = rep(1, n), comment.char = "")
+x <- as.character(do.call(rbind, strsplit(readLines("Input/day06.txt"), "")))
+n <- as.integer(sqrt(length(x)))
+rw <- rep.int(seq_len(n), n)
+cl <- rep(seq_len(n), each = n)
 
-co <- apply(which(x != "#", arr.ind = TRUE), 1, \(z) z[1] *1i + z[2]) 
+obs <- which(x == "#")
+obs_rw <- split(obs, rw[obs])
+obs_cl <- split(obs, cl[obs])
 
-pos_vec <- sum(which(x == "^", arr.ind = TRUE)[1, ] * c(1i, 1))
-dir <- -1i
+dir_vec <- c(-1L, n, 1L, -n)
 
+idx_k <- function(k) {
+  .rw <- rw[k]
+  .cl <- cl[k]
+  
+  obs2 <- obs_cl[[.cl]]
+  res_ud <-  c(max(obs2[obs2 <= k], -2L) + 1L, min(obs2[obs2 >= k], 1e5L + 1L) - 1L)
+  
+  obs3 <- obs_rw[[.rw]]
+  res_lr <- c(max(obs3[obs3 <= k], -1L - n) + n, min(obs3[obs3 >= k], 1e5L + n) - n)
+  
+  c(res_ud, res_lr)[c(1L, 4L, 2L, 3L)] # up, right, down, left
+}
+
+lookup <- sapply(seq_len(n^2), idx_k)
+
+pos <- which(x == "^")
+path <- pos
+
+k <- 1L
 while (TRUE) {
-  
-  co2 <- if (Re(dir) == 0) co[Re(co) == Re(pos)] else co[Im(co) == Im(pos)]
-  pos <- pos_vec[length(pos_vec)]
-  n_steps <- Position(\(k)! (pos + k*dir) %in% co2, 1:130) - 1L
-  pos_vec <- c(pos_vec, pos + dir * seq_len(n_steps))
-  pos <- pos + n_steps * dir
-  pos2 <- pos + dir
-  
-  
-  if (Re(pos2) < 1 || Re(pos2) > n || Im(pos2) < 1 || Im(pos2) > n) {
-    break
-  } else {
-    dir <- dir * 1i
-  }
+  pos <- c(lookup[k, pos[1]], pos)
+  if (pos[1] == -1 || pos[1] == 1e5)  break
+  path <- c(path, seq(pos[2], pos[1], by = dir_vec[k])[-1])
+  k <- k %% 4L + 1L
 }
 
-length(unique(pos_vec))
+# finish path
+pos2 <- arrayInd(pos[2], c(n, n))
+lng <- if (k == 1) pos2[1] else if (k == 3) 131 - pos2[1] else if (k == 2) pos2[2] else 131 - pos2[2]
+final_path <- seq(pos[2], length.out = lng, by = dir_vec[k])[-1]
+path <- c(path, final_path)
+length(unique(path))
 
-f <- function(z0) {
-  co <- co[co != z0]
-  pos <- pos_vec[1]
-  pos_vec <- pos
-  dir <- -1i
-  co2 <- co[Re(co) == Re(pos_pec)]
-  turn_vec <- complex()
-  dir_vec <- complex()
-  check_loop <- FALSE
+# part 2-------------
+
+## update lookup-----
+update_lookup <- function(k) {
+  
+  left  <- which(rw == rw[k] & cl > cl[k])
+  right <- which(rw == rw[k] & cl < cl[k])
+  up    <- which(cl == cl[k] & rw > rw[k]) 
+  down  <- which(cl == cl[k] & rw < rw[k]) 
+  lookup_new <- lookup
+  lookup_new[4, left]  <- pmax.int(lookup[4, left], k + n)
+  lookup_new[2, right] <- pmin.int(lookup[2, right], k - n)
+  lookup_new[1, up]    <- pmax.int(lookup[1, up], k + 1L)
+  lookup_new[3, down]  <- pmin.int(lookup[3, down], k - 1L)
+  
+  return(lookup_new)  
+}
+
+
+check_loop <- function(pos0, dir0) {
+  lookup2 <- update_lookup(pos0 + dir0)
+  k <- which(dir_vec == dir0)
   
   while (TRUE) {
-    
-    n_steps <- Position(\(k)! (pos + k*dir) %in% co2, 1:n) - 1L
-    pos_vec <- c(pos_vec, pos + dir * seq_len(n_steps))
-    pos <- pos + n_steps * dir
-    pos2 <- pos + dir
-    if (pos2 == z0) check_loop <- TRUE
-    
-    if (Re(pos2) < 1 || Re(pos2) > n || Im(pos2) < 1 || Im(pos2) > n) {
-      break
-    } else {
-      if (check_loop)  {
-        if(length(turn_vec) > 4 && pos %in% turn_vec) {
-          if (any(dir_vec[pos == turn_vec] == dir)) return(TRUE)
-        } 
-        turn_vec <- c(turn_vec, pos)
-        dir_vec <- c(dir_vec, dir)
-      }
-      dir <- dir * 1i
-      co2 <- if (Re(dir) == 0) co[Re(co) == Re(pos)] else co[Im(co) == Im(pos)]
-    }
+    pos0 <- c(lookup2[k, pos0[1]], pos0)
+    if (pos0[1] == -1 || pos0[1] == 1e5)  return(0)
+    if (sum(pos0 == pos0[1]) > 3L) return(1)
+    k <- k %% 4L + 1L
   }
-  
-  return(FALSE)
 }
 
-sum(sapply(unique(pos_vec)[-1], f))
+unique_pos <- cbind(path[-length(path)], diff(path))
+unique_pos <- unique_pos[!duplicated(unique_pos[,1] + unique_pos[,2]), ]
 
 
-
-
-go <- function(pos, dir, co, part1 = TRUE) {
-  pos_vec <- pos 
-  dir_vec <- dir
-  loop_count <<- 0L
-  
-  while (TRUE) {
-    
-    co2 <- if (Re(dir) == 0) co[Re(co) == Re(pos)] else co[Im(co) == Im(pos)]
-    n_steps <- Position(\(k)! (pos + k*dir) %in% co2, 1:130) - 1L
-   
-    new_pos <- pos + dir * seq_len(n_steps)
-    
-    if (!part1) {
-      s <- sapply(new_pos[-n_steps], \(p) go(p, dir * 1i, co[co != p + dir]))
-    }
-   
-    pos_vec <- c(pos_vec, new_pos)
-    dir_vec <- c(dir_vec, rep(dir, n_steps))
-    pos <- pos + n_steps * dir
-    pos2 <- pos + dir
-    dir <- dir * 1i 
-    
-    if (!part1 && length(pos_vec[dir_vec == dir & pos_vec == pos + dir]) > 0L) {
-        loop_count <<- loop_count + 1L
-        return(NULL)
-    }
-
-    if (Re(pos2) < 1 || Re(pos2) > n || Im(pos2) < 1 || Im(pos2) > n) break
-  }
-  
-  if (part1) return(cbind(pos_vec, dir_vec)) else return(NULL)
-  
-}
-
-res1 <- go(sum(which(x == "^", arr.ind = TRUE)[1, ] * c(1i, 1)), -1i, co, F)
-
-
-length(unique(res1[,1]))
-
-head(cbind(res1, pos_vec), 20)
+sum(apply(unique_pos, 1, \(x) check_loop(x[1], x[2])))
