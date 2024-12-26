c02b4a36f6615863a4bcb3586b467c0093b98bce
Cetin_G <ceting@gmx.at>
2024-12-26 23:25:50 +0100

    Improved day 20:
    - reduced runtime by 50%

--- day20.R
+++ day20.R
@@ -1,41 +1,41 @@
 data20 <- as.character(do.call(rbind, strsplit(readLines("Input/day20.txt"), "")))
 n <- as.integer(sqrt(length(data20)))
 
-wall <- which(data20 == "#")
-start <- which(data20 == "S")
-end <- which(data20 == "E")
+co <- rep(seq_len(n), each = n) + rep.int(seq_len(n), n) * 1i
 
-tmp <- c(-1, 1, n, -n)
-idx_k <- function(k) k + tmp
+wall <- co[data20 == "#"]
+wall_rw <- split(wall, Im(wall))
+wall_cl <- split(wall, Re(wall))
 
-lookup <- lapply(seq_len(n^2), idx_k)
-
-
-#normal path---------
-
+# construct path--------
 path <- integer(n^2 - length(wall))
-path[1] <- start
+path[1] <- co[data20 == "S"]
+
+dir_vec <- c(-1, 1, 1i, -1i)
+dir <- dir_vec[which(!(path[1] + dir_vec) %in% wall)]
+wall2 <- if (Re(dir) == 0) wall_cl[[Re(path[1])]] else wall_rw[[Im(path[1])]]
 
 for(k in 2:length(path)) {
-  nxt <- lookup[[path[k - 1]]]
-  path[k] <- nxt[(!nxt %in% wall) & nxt != c(path[k - 2], 0)[1]]
-  
+  if ((path[k - 1] + dir) %in% wall2) {
+    wall2 <- if (Re(dir) == 0L) wall_rw[[Im(path[k - 1L])]] else wall_cl[[Re(path[k - 1L])]]
+    dir_idx <- .Internal(which(!(path[k - 1] + dir * c(-1i, 1i)) %in% wall2))
+    dir <- dir * c(-1i, 1i)[dir_idx]
+  }
+  path[k] <- path[k - 1] + dir
 }
 
-path_co <- apply(arrayInd(path, .dim = c(n, n)), 1, \(x) x[1] * 1i + x[2]) 
 
-
-compute_cheat <- function(i, max_cheat) {
-  p0 <- path_co[i]
-  p1 <- path_co[-seq_len(i)]
+compute_cheat <- function(i) {
+  p0 <- path[i]
+  p1 <- path[-seq_len(i + 100L)]
   l1_d <- abs(Re(p1) - Re(p0)) + abs(Im(p1) - Im(p0))
+  idx <- l1_d <= 20
+  l1_d <- l1_d[idx]
+  l1_d <- l1_d[seq_along(p1)[idx] >= l1_d]
   
-  sum(l1_d <= max_cheat & seq_along(p1) - l1_d >= 100)
-  
+  c(sum(l1_d <= 2), length(l1_d)) 
 }
 
-#part 1 ----------
-sum(sapply(seq_along(path_co[-1]), compute_cheat, max_cheat = 2L))
+#part 1 and 2 ----------
+rowSums(sapply(seq_along(path[-seq_len(100)]), compute_cheat))
 
-# part 2------------
-sum(sapply(seq_along(path_co[-1]), compute_cheat, max_cheat = 20L))
